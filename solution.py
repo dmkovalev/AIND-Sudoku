ea758e8eb360bcb89de419b85dc634ae5fa56970
Dmitry Kovalev <DKovalev3@slb.com>
2017-02-20 09:46:09 +0300

    add functions from lectur

--- solution.py
+++ solution.py
@@ -36,7 +36,12 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    s = dict(zip(boxes,grid))
+    for key, value in s.items():
+        if s[key] == '.':
+            s[key] = '123456789'
+
+    return s
 
 def display(values):
     """
@@ -50,13 +55,61 @@ def eliminate(values):
     pass
 
 def only_choice(values):
-    pass
+    for unit in unitlist:
+        sub = ''
+        for k in unit:
+            sub += values[k]
+        con = [l for l in cols if sub.count(l) == 1]
+        for k in unit:
+            for el in con:
+                if el in values[k]:
+                    values[k] = el
+    return values
 
 def reduce_puzzle(values):
-    pass
+    solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+    # Your code here: Use the Eliminate Strategy
+    values = eliminate(values)
+    # Your code here: Use the Only Choice Strategy
+    values = only_choice(values)
+    # Check how many boxes have a determined value, to compare
+    solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+    # If no new values were added, stop the loop.
+    stalled = solved_values_before == solved_values_after
+    # Sanity check, return False if there is a box with zero available values:
+    if len([box for box in values.keys() if len(values[box]) == 0]):
+        return False
+    return values
 
 def search(values):
-    pass
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    # Choose one of the unfilled squares with the fewest possibilities
+    key = ''
+
+    if values is False:
+        return False
+
+    if all(len(values[s]) == 1 for s in boxes):
+        return values ## Solved!
+
+    for k,v in values.items():
+        if len(v) > 1:
+            key = k
+
+    for k,v in values.items():
+        if len(values[key]) > len(v) and len(v) > 1:
+            key = k
+
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for v in values[key]:
+        new_v = values.copy()
+        new_v[key] = v
+        r = search(new_v)
+        if r:
+            return r
+    # If you're stuck, see the solution.py tab!
 
 def solve(grid):
     """
